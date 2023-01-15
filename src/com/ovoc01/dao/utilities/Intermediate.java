33e5d8ff12d521fe9769e28e3da2b76e59e7d294
ovoc01 <spotifymirindra@gmail.com>
2023-01-15 18:24:37 +0300

    modif

---
+++ src/com/ovoc01/dao/utilities/Intermediate.java
@@ -0,0 +1,24 @@
+package com.ovoc01.dao.utilities;
+/**
+*@author ovoc01
+* */
+
+import java.sql.ResultSet;
+
+/**
+* Intermediate class for object management
+*
+* */
+@SuppressWarnings("unused")
+public class Intermediate {
+    /**
+     * Create an object which is an instance of reference using the ResultSet
+    *@param reference object to referee
+     * @param rs a ResultSet which need to be transforms
+     * @return an instance of reference object
+    * */
+    public static Object createObject(Object reference, ResultSet rs){
+        return null;
+    }
+
+}
